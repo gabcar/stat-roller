1c4a9397353f6e6db97dc96194bd4711bb1e7df3
Gabriel Carrizo <gab.carrizo@live.com>
2019-09-02 11:17:09 +0200

    Updates

---
+++ precache-manifest.4ea095f9cbaf707f228ed15518e8f931.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ed70e08a3b47dabd705046f2f73d6b",
+    "url": "/stat-roller/index.html"
+  },
+  {
+    "revision": "4e37c7f5edd3dcd87f2c",
+    "url": "/stat-roller/static/css/main.1e26e9a6.chunk.css"
+  },
+  {
+    "revision": "61d04946c08972cdcb05",
+    "url": "/stat-roller/static/js/2.09d912ee.chunk.js"
+  },
+  {
+    "revision": "4e37c7f5edd3dcd87f2c",
+    "url": "/stat-roller/static/js/main.889dfb99.chunk.js"
+  },
+  {
+    "revision": "f8382c936427034c1707",
+    "url": "/stat-roller/static/js/runtime~main.b3213c3e.js"
+  }
+]);
