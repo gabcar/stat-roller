966a333401f1cf0c7579b1b886e717a24285ef51
Gabriel Carrizo <gab.carrizo@live.com>
2019-09-02 13:12:16 +0200

    Updates

--- precache-manifest.0a90c86cd6669b202cfbc74f762416ea.js
+++ precache-manifest.0a90c86cd6669b202cfbc74f762416ea.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d8efe1f5932f921c19cbb6cf87df92cc",
+    "revision": "89c1b41cc9d09093e132beee51f099a1",
     "url": "/stat-roller/index.html"
   },
   {
-    "revision": "ec3568d61c0cf210bde3",
-    "url": "/stat-roller/static/css/main.8619560e.chunk.css"
+    "revision": "c4cf318b60ba9f57852d",
+    "url": "/stat-roller/static/css/main.f47bd527.chunk.css"
   },
   {
     "revision": "61d04946c08972cdcb05",
     "url": "/stat-roller/static/js/2.09d912ee.chunk.js"
   },
   {
-    "revision": "ec3568d61c0cf210bde3",
+    "revision": "c4cf318b60ba9f57852d",
     "url": "/stat-roller/static/js/main.ce0cdfe2.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/stat-roller/precache-manifest.62077cc99f51791636bb57f29e50d56f.js"
+  "/stat-roller/precache-manifest.0a90c86cd6669b202cfbc74f762416ea.js"
 );
 
 self.addEventListener('message', (event) => {
